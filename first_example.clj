bbf67558b1b806f1649cef35f84b56754f6f059b
joelsonnSantos <joelsonn.santos@gmail.com>
2019-04-24 11:44:18 -0300

    teste para somar 1o milhões de elementos em uma lista usando a função reduce

--- first_example.clj
+++ first_example.clj
@@ -1,10 +1,11 @@
 (ns clojure.examples.tests
 	(:gen-class))
 
-;; 
+;;
 (defn sum-two-elements [element1 element2]
     (println  (str (+ element1 element2))))
 (sum-two-elements 1 3)
 
-;; print ten integer random elements
-(println (take 10 (repeatedly (fn [] (int (rand 100))))))
+(def elements (take 10000000 (repeatedly (fn [] (int (rand 10000000))))))
+;; compute timestamp of reduce primite math operation
+(def timestp (time (reduce + elements)))
